6070eab572aee8d556ce2a1963d10905f308c5f7
umaselvaraj00 <106576027+umaselvaraj00@users.noreply.github.com>
2022-12-18 22:31:27 +0530

    Add files via upload

--- Pagination.js
+++ Pagination.js
@@ -1,63 +1,94 @@
 import axios from 'axios';
-// import Page from './Page';
 import React, { useEffect, useState } from 'react'
 export default function Pagination() {
     const[value,setValue]=useState([]);
     const[search,setSearch]=useState("");
+    const[searchElem]=useState(["id","name","age"])
      const[filtered,setFilterd]=useState([])
-     const[sorted,setSorted]=useState(true);
+     const[sorted,setSorted]=useState("Ascending");
+     const [number,setNumber]=useState(1);
+
+     const PostPerPage=10;
 
     useEffect(()=>{
         axios.get("http://localhost:4000/pagination")
         .then(response=>{
          setValue(response.data);
-        //  console.log(response.data);
-         setFilterd(value.filter(data=>{return data.name.toLowerCase().includes(search.toLowerCase());}))
-        });
+        //  setFilterd(value.filter(data=>{return data.name.toLowerCase().includes(search.toLowerCase());}))
+        // });
     },[search,value]);
+  }
+
+    const data=Object.values(value);
+
+    return search(value){
+      return value.filter((value) => {
+        if(value.region==filtered){
+          return searchElem.some((newItem) => {
+            return (
+              value[newItem] .toString() .toLowerCase() .indexOf(value.toLowerCase()) > -1);});
+            }else if (filtered=="All"){
+              return searchElem.some((newItem)=>){
+                  value[newItem].toString().toLowerCase()
+                  .indexOf(search.toLowerCase()> -1);)
+              });
+
+              
+            }
+        });
+    }
+  }
+    
+      
+          
+              
+                 
+                 
+                 
+          
+
+     const lastPage = number * PostPerPage;
+     const firstPage = lastPage - PostPerPage;
+     const currentPage = value.slice(firstPage, lastPage);
+     const pageNumber = [];
   
-
+     for (let i = 1; i <= Math.ceil(value.length / PostPerPage); i++) {
+           pageNumber.push(i);
+         }
+      const changePage = (pageNumber) => {
+           setNumber(pageNumber);
+      }
     const handleSort=(col)=>{
-        if(sorted==="Asending"){
-            const sorting=[...value].sort((a,b)=>a[col].toLowerCase( ) > b[col].toLowerCase()?1: -1
+        if(sorted==="Ascending"){
+            const sorting=[...value].sort((a,b)=>a[col].toLowerCase() > b[col].toLowerCase() ? 1: -1
             );
             setValue(sorting);
-            setSorted("Desending");
+            setSorted("Descending");
 
-        }if(sorted==="Desending")
+        }if(sorted==="Descending")
         {
-            const sorting=[...value].sort((a,b)=>a[col].toLowerCase( ) < b[col].toLowerCase() ?1: -1
+            const sorting=[...value].sort((a,b)=>a[col].toLowerCase() < b[col].toLowerCase() ? 1: -1
             );
             setValue(sorting);
-            setSorted("Asending");
+            setSorted("Ascending");
 
         }
     }
-        
-
-
-return (
+  return (
     <div>
-    {/* <h5>Sort</h5>
-    <select >
-    <option disabled value='none'>sort</option>
-    <option value='id' onclick={handleSort}>Id</option>
-    <option value='name' onclick={handleSort}>Name</option>
-    <option value='age' onclick={handleSort}>Age</option>
-    </select> */}
-    {/* <button onChange={handleSubmit}>Search</button> */}
+       
     <input type="text" placeholder="search here..." value={search} onInput={(e)=>setSearch(e.target.value)}></input>
     <table>
     <thead>
         <tr>
-            <th onClick={()=>handleSort("id")}>S.No </th>
-            <th onClick={()=>handleSort("name")}>Name </th>
-            <th onClick={()=>handleSort("email")}>Email </th>
-            <th onClick={()=>handleSort("age")}>Age </th>
+            <th onClick={()=>handleSort("id")}>S.No ↑↓   </th>
+            <th onClick={()=>handleSort("name")}>Name ↑↓ </th>
+            <th onClick={()=>handleSort("email")}>Email ↑↓ </th>
+            <th onClick={()=>handleSort("age")}>Age ↑↓ </th>
         </tr>
         </thead>
         <tbody>
-        {filtered.map((value,index)=>(
+        {currentPage.map((value,index)=>(
             <tr key={index}>
             <td>{value.id}</td>
             <td>{value.name}</td>
@@ -69,10 +100,16 @@ return (
            
          </tbody>
         </table>
+        <button onClick={()=>setNumber(number-1)}>Previous</button>
+          {pageNumber.map((ele)=>{
+               return(<button onClick={()=>changePage(ele)}>{ele}</button>
+        );
+        })}
+        <button onClick={()=>setNumber(number+1)}>Next</button> 
+        
             
-       {/* <Page data={value}> </Page>  */}
+       
 
         </div>
   )
 }
-
